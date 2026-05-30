99f00a3520bc0233e324bda1dd221fe66e42f761
harpodiffpriv <reviewpaper.repo@gmail.com>
2026-05-30 13:57:03 -0400

    Update 2D.py

--- 2D.py
+++ 2D.py
@@ -3,14 +3,12 @@ import plotly.express as px
 
 df = pd.read_excel("scores.xlsx")
 manual = {
-    "2":      "bottom right",
-    "19":      "middle left",
-    "74":      "bottom right",
-    "64":      "bottom left",
-    "36":      "middle left",
-    "44":      "bottom left",
-    "45":      "bottom left"
-}
+    "4":      "bottom right",
+    "18":      "middle left",
+    "12":      "bottom right",
+    "53":      "bottom left",
+    "74":      "bottom left",
+
 
 positions = [manual.get(str(p), "top center") for p in df["Paper"]]  
 
@@ -61,44 +59,6 @@ fig.update_layout(
         tickvals=[0, 0.25, 0.5, 0.75, 1.0],
         ticktext=["Bad", "Low", "Medium", "Good", "Best"]
     )
-)
-
-# Find the row for Paper 70
-target_paper = "70"
-
-row = df[df["Paper"].astype(str) == target_paper].iloc[0]
-
-x0 = row["Privacy"]
-y0 = row["Utility"]
-s0 = row["Safety"]
-
-fig.add_annotation(
-    x=x0,
-    y=y0,
-    text=(
-        f"<b>Paper: {target_paper}</b><br>"
-        f"Privacy: {x0:.2f}<br>"
-        f"Utility: {y0:.2f}<br>"
-        f"Safety: {s0:.2f}"
-    ),
-    showarrow=False,
-    xanchor="left",
-    yanchor="bottom",
-    xshift=10,
-    yshift=10,
-    align="left",
-    bgcolor="#B2182B",      
-    bordercolor="#B2182B",
-    borderwidth=1,
-    font=dict(color="white", size=12),
-    opacity=0.95
-)
-
-fig.write_image(
-    "plot_with_hover_label.png",
-    width=700,
-    height=600,   
-    scale=1       
 )
 
 fig.show()
